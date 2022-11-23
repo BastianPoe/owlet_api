fb3df9de79900b25a6c8a37995c83ac16eed65a1
antgiant <antgiant@gmail.com>
2022-11-23 17:56:58 -0500

    Login 2.0 Beta URLs fixed Improved Error Messages Squashed a few other bugs

--- owlet_api/owletapi.py
+++ owlet_api/owletapi.py
@@ -16,8 +16,8 @@ class OwletAPI():
 
     owlet_login_url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCBJ_5TRcPz_cQA4Xdqpcuo9PE5lR8Cc7k'
     owlet_login_token_provider_url = 'https://ayla-sso.owletdata.com/mini/'
-    base_user_url = 'https://user-field.aylanetworks.com/users/'
-    base_properties_url = 'https://ads-owlue1.aylanetworks.com/api/v1/'
+    base_user_url = 'https://ads-owlue1.aylanetworks.com/api/v1/'
+    base_properties_url = 'https://ads-owlue1.aylanetworks.com/apiv1/'
 
     def __init__(self, email=None, password=None):
         """Initialize OwletAPI, with email and password as opt. arguments."""
@@ -72,34 +72,40 @@ class OwletAPI():
             )
         except RequestException:
             raise OwletTemporaryCommunicationException(
-                '(Step 1) Login request failed - no response')
+                '(Step 1) Login request failed - no response', result)
 
         # Login failed
-        if result.status_code == 401:
+        if result.status_code == 400:
+            if result.text.__contains__('EMAIL_NOT_FOUND'):
+                raise OwletPermanentCommunicationException(
+                    'Login failed, bad username', result)
+            if result.text.__contains__('INVALID_PASSWORD'):
+                raise OwletPermanentCommunicationException(
+                    'Login failed, bad password', result)
             raise OwletPermanentCommunicationException(
-                '(Step 1) Login failed, check username and password')
+                'Login failed, check username and password', result)
         if result.status_code != 200:
             raise OwletTemporaryCommunicationException(
-                '(Step 1) Login request failed - status code')
+                'Login request failed - status code (' + str(result.status_code) + ') - (Step 2 of 3)', result)
 
         # Login seems to be ok, extract json
         try:
             json_result = result.json()
         except JSONDecodeError:
             raise OwletTemporaryCommunicationException(
-                '(Step 1) Server did not send valid json')
+                'Server did not send valid json (Step 1 of 3)', json_result)
 
         if ('idToken' not in json_result) or \
            ('refreshToken' not in json_result) or \
-           ('expires_in' not in json_result):
+           ('expiresIn' not in json_result):
             raise OwletTemporaryCommunicationException(
-                '(Step 1) Server did not send access token')
+                'Server did not send id token (Step 1 of 3)', json_result)
 
         self._owlet_id_token = json_result['idToken']
         self._owlet_refresh_token = json_result['refreshToken']
-        self._owlet_id_token_expiry_time = time.time() + json_result['expires_in']
+        self._owlet_id_token_expiry_time = time.time() + int(json_result['expiresIn'])
 
-        """Step 2: Retreive Ayla Login Token."""
+        """Step 2: Retrieve Ayla Login Token."""
         login_headers = {
             'Accept': 'application/json',
             'authorization': self._owlet_id_token
@@ -108,33 +114,30 @@ class OwletAPI():
         login_url = self.owlet_login_token_provider_url
 
         try:
-            result = requests.post(
+            result = requests.get(
                 login_url,
                 headers=login_headers,
                 timeout=5
             )
         except RequestException:
             raise OwletTemporaryCommunicationException(
-                '(Step 2) Login request failed - no response')
+                'Login request failed - no response (Step 2 of 3)', result)
 
         # Login failed
-        if result.status_code == 401:
-            raise OwletPermanentCommunicationException(
-                '(Step 2) Login failed, check username and password')
         if result.status_code != 200:
             raise OwletTemporaryCommunicationException(
-                '(Step 2) Login request failed - status code')
+                'Login request failed - status code (' + str(result.status_code) + ') - (Step 2 of 3)', result)
 
         # Login seems to be ok, extract json
         try:
             json_result = result.json()
         except JSONDecodeError:
             raise OwletTemporaryCommunicationException(
-                '(Step 2) Server did not send valid json')
+                'Server did not send valid json (Step 2 of 3)', json_result)
 
         if ('mini_token' not in json_result):
             raise OwletTemporaryCommunicationException(
-                '(Step 2) Server did not send access token')
+                'Server did not send mini token (Step 2 of 3)', json_result)
 
         self._owlet_mini_token = json_result['mini_token']
 
@@ -147,14 +150,12 @@ class OwletAPI():
         login_url = self.base_user_url + 'token_sign_in.json'
 
         login_payload = {
-            'body': {
-                'token': self._owlet_mini_token,
-                'app_id': 'owa-rg-id',
-                'app_secret': 'owa-dx85qljgtR6hmVflyrL6LasCxA8',
-                'headers': {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
+            'token': self._owlet_mini_token,
+            'app_id': 'owa-rg-id',
+            'app_secret': 'owa-dx85qljgtR6hmVflyrL6LasCxA8',
+            'headers': {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             }
         }
 
@@ -167,28 +168,25 @@ class OwletAPI():
             )
         except RequestException:
             raise OwletTemporaryCommunicationException(
-                '(Step 3) Login request failed - no response')
+                'Login request failed - no response (Step 3 of 3)', result)
 
         # Login failed
-        if result.status_code == 401:
-            raise OwletPermanentCommunicationException(
-                '(Step 3) Login failed, check username and password')
         if result.status_code != 200:
             raise OwletTemporaryCommunicationException(
-                '(Step 3) Login request failed - status code')
+                'Login request failed - status code (' + str(result.status_code) + ') - (Step 3 of 3)', result)
 
         # Login seems to be ok, extract json
         try:
             json_result = result.json()
         except JSONDecodeError:
             raise OwletTemporaryCommunicationException(
-                '(Step 3) Server did not send valid json')
+                'Server did not send valid json(Step 3 of 3)', json_result)
 
         if ('access_token' not in json_result) or \
            ('refresh_token' not in json_result) or \
            ('expires_in' not in json_result):
             raise OwletTemporaryCommunicationException(
-                '(Step 3) Server did not send access token')
+                'Server did not send access token(Step 3 of 3)', json_result)
 
         self._auth_token = json_result['access_token']
         self._refresh_token = json_result['refresh_token']
@@ -237,17 +235,17 @@ class OwletAPI():
             )
         except RequestException:
             raise OwletTemporaryCommunicationException(
-                'Server request failed - no response')
+                'Server request failed - no response', result)
 
         if result.status_code != 200:
             raise OwletTemporaryCommunicationException(
-                'Server request failed - status code')
+                'Server request failed - status code', result)
 
         try:
             json_result = result.json()
         except JSONDecodeError:
             raise OwletTemporaryCommunicationException(
-                'Server did not send valid json')
+                'Server did not send valid json', json_result)
 
         self._devices = []
 
